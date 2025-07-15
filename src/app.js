a8d8935a29e5b9ecb02693631d0d5f28a9af1837
Huzaifa Haleem <huzaifahaleem682@gmail.com>
2025-07-16 01:07:48 +0500

    bug fix

--- src/app.js
+++ src/app.js
@@ -22,27 +22,27 @@ import userRouter from "./routes/user.route.js";
 app.use("/api/v1/users", userRouter);
 
 //import video route
-import videoRouter from "./routes/video.route.js"
-app.use("/api/v1/videos", videoRouter)
+import videoRouter from "./routes/video.route.js";
+app.use("/api/v1/videos", videoRouter);
 
 //import comment route
-import commentRouter from "./routes/comment.route.js"
-app.use("/api/v1/comment", commentRouter)
+import commentRouter from "./routes/comment.route.js";
+app.use("/api/v1/comment", commentRouter);
 
 //import like route
-import likeRouter from "./routes/like.route.js"
-app.use("/api/v1/likes", likeRouter)
+import likeRouter from "./routes/like.route.js";
+app.use("/api/v1/likes", likeRouter);
 
 //import tweet route
-import tweetRouter from "./routes/tweet.route.js"
-app.usr("api/v1/tweet", tweetRouter)
+import tweetRouter from "./routes/tweet.route.js";
+app.use("/api/v1/tweet", tweetRouter);
 
 //import subscription route
-import subscriptionRouter from "./routes/suscription.route.js"
-app.use("/api/v1/subscription", subscriptionRouter)
+import subscriptionRouter from "./routes/suscription.route.js";
+app.use("/api/v1/subscription", subscriptionRouter);
 
 //import playlist route
-import playlistRouter from "./routes/playlist.route.js"
-app.use("/api/v1/playlist", playlistRouter)
+import playlistRouter from "./routes/playlist.route.js";
+app.use("/api/v1/playlist", playlistRouter);
 
 export { app };
--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -1,4 +1,31 @@
 import asyncHandler from "../utils/asyncHandler.js";
 import ApiError from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-import {Playlist} from "../models/playlist.model.js"
+import { Playlist } from "../models/playlist.model.js";
+
+//createPlaylist
+const createPlaylist = asyncHandler(async (req, res) => {
+  const { name, description, videos } = req.body;
+  if ([name, description].some((field) => !field || field.trim() === "")) {
+    throw new ApiError(400, "All field Required");
+  }
+  if (videos && !Array.isArray(videos)) {
+    throw new ApiError(400, "Videos must be an array of video ids");
+  }
+
+  const playlist = await Playlist.create({
+    name,
+    description,
+    videos: videos || [],
+    owner: req.user._id,
+  });
+  if (!playlist) {
+    throw new ApiError(500, "Something went wrong");
+  }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, { playlist }, "Playlist created"));
+});
+
+
+export { createPlaylist, getUserPlaylists };
